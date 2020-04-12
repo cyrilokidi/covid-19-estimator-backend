2594005d192614dfb2fb4f4c29693f79a631ac01
Cyril Okidi <okidicyril@gmail.com>
2020-04-12 17:23:48 +0300

    update response content-type for audit logs and error responses

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ const logsResponse = (fs, path) => async (req, res, next) => {
       data += `${v}\t\t${audits[v].originalUrl}\t\tdone in ${audits[v].duration} seconds`;
     });
 
-    res.status(200).setHeader('Content-Type', 'text/html');
+    res.status(200).setHeader('Content-Type', 'text/plain');
     res.send(data);
   } catch (e) {
     next(e);
@@ -89,7 +89,7 @@ const logsResponse = (fs, path) => async (req, res, next) => {
 
 // Handle route errors.
 const errorHandler = async (err, req, res, next) => {
-  res.status(500).setHeader('Content-Type', 'text/html');
+  res.status(500).setHeader('Content-Type', 'text/plain');
   res.send('Internal server error.');
 };
 
